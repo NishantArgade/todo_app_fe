737d8c71f1df46176789417a5d20705b7b265083
Nishant Argade <nishantargade4579@gmail.com>
2023-06-29 22:29:51 +0530

    env file added

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-export const server_url = "http://localhost:5000/api/v1";
-
+export const server_url = import.meta.env.VITE_REACT_APP_SERVER_URL;
+// console.log(server_url);
 export const Context = createContext({ isAuthenticated: false });
 
 const AppWrapper = () => {
